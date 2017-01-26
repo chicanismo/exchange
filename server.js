7ac7aa0912d40488da4e1760fcbbc1b587c93cc0
El_Gato <albertomadueno@gmail.com>
2017-01-26 10:04:21 -0800

    started feature facebook auth

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+// const passport = require('passport');
+
+// const Strategy = required('passport-facebook').Strategy;
+
+// passport.use(new Strategy({
+//     clientID: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     callbackURL: 'http://localhost:3000/login/facebook/return',
+//     profileFields: ['email', 'displayName', 'id', 'picture.type(large)']
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+    
+//     return cb(null, profile);
+//   }));
+
+// passport.serializeUser(function(user, cb) {
+//   cb(null, user);
+// });
+
+// passport.deserializeUser(function(obj, cb) {
+//   cb(null, obj);
+// });
+
 
 const app = express();
 
@@ -11,5 +34,5 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 const port = process.env.PORT || '3000';
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(' Example app listening on port 3000!');
 });
